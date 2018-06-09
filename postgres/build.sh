3a5169661e845c837faec6c7fb275f570b82ceef
tcressy <taylor_cressy@intuit.com>
2018-06-09 11:21:59 -0700

    Updated centos base

--- postgres/build.sh
+++ postgres/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+DATA_DIR=/data/psql/
+mkdir -p $DATA_DIR
+docker volume create --name psqlvol-tcressy --opt device=$DATA_DIR --opt o=bind
 docker build . -t tcressy/postgres
--- postgres/run.sh
+++ postgres/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-docker run -p 5432:5432 -p 5000:5000 --name postgres-tcressy --env-file ./env-file --mount source=psqlvol-tcressy,target=/var/lib/pgsql \
+# Remove existing docker if exists
+docker kill postgres-tcressy
+docker rm postgres-tcressy
+
+# Run docker for first time
+docker run -p 5432:5432 -p 5000:5000 --name postgres-tcressy \
+    --env-file ./env-file \
+    --mount source=psqlvol-tcressy,target=/var/lib/pgsql \
     tcressy/postgres
